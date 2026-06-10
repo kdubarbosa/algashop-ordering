dffedd612a5533a2ccc1f9f94f55c083cd7c9f5e
Carlos Eduardo Barbosa <kdubpf@gmail.com>
2026-06-10 11:19:22 -0300

    Comit inicial

---
+++ src/main/java/com/algaworks/algashop/ordering/AlgashopOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algashop.ordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgashopOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgashopOrderingApplication.class, args);
+	}
+
+}
